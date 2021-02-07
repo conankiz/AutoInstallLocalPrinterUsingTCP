ea9039f94ca5e60ed7359e34e86283f7d7bb6fb9
conankiz <manhhungbl@gmail.com>
2021-02-07 19:04:56 +0700

    Update AutoAddPrinterLocalTCP.bat
    add Printer Model

--- AutoAddPrinterLocalTCP.bat
+++ AutoAddPrinterLocalTCP.bat
@@ -24,6 +24,7 @@ set DriversFolder="\\192.168.11.250\Source\Driver\Printer\ljP2035-gdi-pnp-win64-
 set Ip_Printer=192.168.11.6
 set NamePrinter="HP LaserJet P2035n"
 set NamePrinterInstall="HP_P2035n"
+set PrinterModel="HP2030.INF"
 
 echo [*] Step 1:
 echo The first Prnmngr.vbs is to delete the printer if you're trying to add a printer with the same name
@@ -35,7 +36,7 @@ Cscript %Printingvbs%\Prnport.vbs -a -r %Ip_Printer% -h %Ip_Printer% -o raw -n 9
 
 echo [*] Step 3:
 echo Installs the printer driver, first getting the printer model and .inf file, then -h for the path to the .dll
-Cscript %Printingvbs%\Prndrvr.vbs -a -m %NamePrinter% -i %DriversFolder%\HP2030.INF -h %DriversFolder%
+Cscript %Printingvbs%\Prndrvr.vbs -a -m %NamePrinter% -i %DriversFolder%\%PrinterModel% -h %DriversFolder%
 
 echo [*]  Step4
 echo Finally giving the printer a name of "PHS - Online", linking the model and printer port
